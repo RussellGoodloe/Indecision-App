f9921856a9ebc6e35fbee1852b8488f645c4166a
Russell Goodloe <russell@ugollc.com>
2020-06-10 10:46:44 -0500

    Working now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,12 @@
+"use strict";
+
 console.log("App.js is running");
 
-//var template = <p>testing jsx from app.js</p>;
-var template = /*#__PURE__*/React.createElement("h1", {
-    id: "someid"
-  }, "Something new");
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App"
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
