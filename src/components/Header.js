330b1889fa4d818623be6176509378a61ceb0260
RussellGoodloe <50248231+RussellGoodloe@users.noreply.github.com>
2020-07-19 15:09:30 -0500

    busted URLs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { NavLink} from 'react-router-dom';
 
-const Header = (props) => (
-        <div className = "header">
-            <div className = "container">
-                <h1 className = "header__title">{props.title}</h1>
-                {props.subtitle && <h2 className = "header__subtitle">{props.subtitle}</h2>}
-            </div>
-            
-        </div>
-    );
-
-Header.defaultProps = {
-    title: 'Indecision App!'
-}
-
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <p>
+            <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink> | 
+            <NavLink to="/create" activeClassName="is-active">New Expense</NavLink> | 
+            <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> | 
+            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        </p>
+    </header>
+);
 export default Header;
