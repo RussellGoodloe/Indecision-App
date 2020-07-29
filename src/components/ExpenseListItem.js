8d5141783d7ff5a2426a979ebe2fc1ba551726c5
RussellGoodloe <50248231+RussellGoodloe@users.noreply.github.com>
2020-07-29 16:41:13 -0500

    formatting from amt and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import moment from 'moment';
+import numeral from 'numeral';
 import { NavLink } from 'react-router-dom';
 
 
@@ -10,7 +11,11 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
                 {description}
             </NavLink>
         </h3>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+              -  
+            {moment(createdAt).format('MMMM do, YYYY')}
+        </p>
     </div>
 ); 
 
